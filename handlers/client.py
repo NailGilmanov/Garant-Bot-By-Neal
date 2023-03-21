8026adc6d19069d31f28f5ccf584a7a1548def2b
NailGilmanov <NailGIlmanov27@mail.ru>
2023-03-21 23:01:42 +0300

    FSMMakeDeal, FSMEnterDeal

--- handlers/client.py
+++ handlers/client.py
@@ -1,6 +1,79 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
 from create_bot import bot
 from keys import kb_client
+
+
+class FSMMakeDeal(StatesGroup):
+    des = State()
+    val = State()
+    price = State()
+    login = State()
+    password = State()
+
+# @dp.message_handler(commands='Создать_сделку', state=None)
+async def start_deal(message: types.Message):
+    await FSMMakeDeal.des.set()
+    await message.reply("Отправь описание товара")
+
+
+# @dp.message_handler(state=FSMMakeDeal.des)
+async def set_des(message: types.Message, state: FSMMakeDeal):
+    async with state.proxy() as data:
+        data['Description'] = message.text
+    await FSMMakeDeal.next()
+    await message.reply("Введи чем вы будете оплачивать? \nНапример, usd, rub, usdt, ton, btc, eth, bnb, busd, usdc")
+
+
+async def set_val(message: types.Message, state: FSMMakeDeal):
+    async with state.proxy() as data:
+        data['Valute'] = message.text
+    await FSMMakeDeal.next()
+    await message.reply("Введите цену\nНапример, 100")
+
+
+async def set_price(message: types.Message, state: FSMMakeDeal):
+    async with state.proxy() as data:
+        data['Price'] = message.text
+    await FSMMakeDeal.next()
+    await message.reply("Введите логин продаваемого аккаунта")
+
+
+async def set_login(message: types.Message, state: FSMMakeDeal):
+    async with state.proxy() as data:
+        data['Login'] = message.text
+    await FSMMakeDeal.next()
+    await message.reply("Введите пароль продаваемого аккаунта")
+
+
+async def set_password(message: types.Message, state: FSMMakeDeal):
+    async with state.proxy() as data:
+        data['Password'] = message.text
+
+    async with state.proxy() as data:
+        await message.reply(str(data))
+    await state.finish()
+
+
+class FSMEnterDeal(StatesGroup):
+    id_of_deal = State()
+
+
+# @dp.message_handler(commands='Создать_сделку', state=None)
+async def start_enter(message: types.Message):
+    await FSMEnterDeal.id_of_deal.set()
+    await message.reply("Отправь айди сделки")
+
+
+# @dp.message_handler(state=FSMMakeDeal.des)
+async def set_id_of_deal(message: types.Message, state: FSMEnterDeal):
+    async with state.proxy() as data:
+        data['id_of_deal'] = message.text
+
+    async with state.proxy() as data:
+        await message.reply(str(data))
+    await state.finish()
 
 
 # /start, /help
@@ -20,6 +93,28 @@ async def price(message: types.Message):
     )
 
 
+# /faq
+async def faq(message: types.Message):
+    await bot.send_message(
+        message.from_user.id,
+        "Инструкция как пользоваться ботом: \n\nЕсли у вас остались вопросы пишите мне: @absoluteriki"
+    )
+
+
 def register_handlers_client(dp: Dispatcher):
+    # создание услуги
+    dp.register_message_handler(start_deal, commands=["Создать_сделку"], state=None)
+    dp.register_message_handler(set_des, state=FSMMakeDeal.des)
+    dp.register_message_handler(set_val, state=FSMMakeDeal.val)
+    dp.register_message_handler(set_price, state=FSMMakeDeal.price)
+    dp.register_message_handler(set_login, state=FSMMakeDeal.login)
+    dp.register_message_handler(set_password, state=FSMMakeDeal.password)
+
+    # вход в услугу
+    dp.register_message_handler(start_enter, commands=["Войти_в_сделку"], state=None)
+    dp.register_message_handler(set_id_of_deal, state=FSMEnterDeal.id_of_deal)
+
+    # прочее
     dp.register_message_handler(greeting, commands=["start", "help"])
     dp.register_message_handler(price, commands=["Стоимость_услуг"])
+    dp.register_message_handler(faq, commands=["Помощь(FAQ)"])
--- keys/client_kb.py
+++ keys/client_kb.py
@@ -5,7 +5,7 @@ b2 = KeyboardButton("/Создать_сделку")
 b3 = KeyboardButton("/Стоимость_услуг")
 b4 = KeyboardButton("/Баланс")
 b5 = KeyboardButton("/Статистика_сделок")
-b6 = KeyboardButton("Помощь (FAQ)")
+b6 = KeyboardButton("/Помощь(FAQ)")
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 kb_client.add(b1).insert(b2).add(b3).insert(b4).add(b5).insert(b6)
