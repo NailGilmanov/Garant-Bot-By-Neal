c9a542bc3b48d9b9accb8e877de9aa258f378349
NailGilmanov <NailGIlmanov27@mail.ru>
2023-03-22 20:30:58 +0300

    поплнение баланса

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -26,3 +26,11 @@ async def sql_get_balance(message):
 async def sql_get_user_balance(id, name):
     for ret in cur.execute(f"SELECT * FROM users WHERE id == {id}").fetchall():
         await bot.send_message(id, f"Пользователь: \n{name} [{id}]\n\nБаланс:\nUSD: {ret[1]}\nRUB: {ret[2]}\nUSDT: {ret[3]}\nTON: {ret[4]}\nBTC: {ret[5]}\nETH: {ret[6]}\nBNB:{ret[7]}\nBUSD: {ret[8]}\nUSDC: {ret[9]}")
+
+
+async def add_to_balance(id, val, amount):
+    last_amount = ''
+    for ret in cur.execute(f"SELECT {val} FROM users WHERE id == {id}").fetchall():
+        last_amount = ret[0]
+    cur.execute(f'UPDATE users SET {val} = {str(float(last_amount) + float(amount))} WHERE id == {id}')
+    base.commit()
--- handlers/admin.py
+++ handlers/admin.py
@@ -11,12 +11,57 @@ class FSMAdmin(StatesGroup):
     id_of_deal = State()
     name = State()
 
+
+class FSMAddSum(StatesGroup):
+    id_of_user = State()
+    val = State()
+    amount = State()
+
+
 # Получение ID модераторов
 # @dp.message_handler(commands=['moderator'], is_chat_admin=True)
 async def make_changes_command(message: types.Message):
     global IDS
     open("admins.txt", 'w').write(str(message.from_user.id))
     await message.reply(f"Админ добавлен \nТекущий список админов: {IDS}")
+
+
+#@dp.message_handler(commands='Добавить_сумму', state=None)
+async def add_sum_start(message: types.Message):
+    if message.from_user.id in IDS:
+        await FSMAddSum.id_of_user.set()
+        await message.reply("Введи id пользователя")
+
+
+async def get_id_to_add(message: types.Message, state: FSMContext):
+    if message.from_user.id in IDS:
+        async with state.proxy() as data:
+            data["id"] = message.text
+        await FSMAddSum.next()
+        await message.reply("Введи валюту \nНапример, usd, rub, usdt, ton, btc, eth, bnb, busd, usdc")
+
+
+async def get_val_to_add(message: types.Message, state: FSMContext):
+    if message.from_user.id in IDS:
+        async with state.proxy() as data:
+            data["val"] = message.text
+        await FSMAddSum.next()
+        await message.reply("Введи сумму\nНапример, 100")
+
+
+async def get_amount_to_add(message: types.Message, state: FSMContext):
+    if message.from_user.id in IDS:
+        async with state.proxy() as data:
+            data["amount"] = message.text
+
+        try:
+            await sqlite_db.add_to_balance(data["id"], data["val"], data["amount"])
+
+            await message.reply(f"Баланс пользователя [{data['id']}] успешно пополнен\n\nПроверить его можно воспользовавшись командой /Узнать_информацию_о_пользователе")
+        except ValueError:
+            await message.reply("Были введены неверные данные\n\nПопытайтесь снова")
+
+        await state.finish()
 
 
 # @dp.message_handler(commands="Узнать_информацию_о_сделке", state=None)
@@ -49,5 +94,10 @@ def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cm_start, commands=["Узнать_информацию_о_пользователе"], state=None)
     dp.register_message_handler(get_id, state=FSMAdmin.id_of_deal)
     dp.register_message_handler(get_name, state=FSMAdmin.name)
+
+    dp.register_message_handler(add_sum_start, commands=["Добавить_сумму"], state=None)
+    dp.register_message_handler(get_id_to_add, state=FSMAddSum.id_of_user)
+    dp.register_message_handler(get_val_to_add, state=FSMAddSum.val)
+    dp.register_message_handler(get_amount_to_add, state=FSMAddSum.amount)
 
     dp.register_message_handler(make_changes_command, commands=['moderator'])
